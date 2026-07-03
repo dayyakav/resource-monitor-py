f154fb8f148f5725bdcd3113f8f797ebfd1c1289
dayyakav <dayyakav@gmail.com>
2026-07-04 02:37:32 +1000

    exception handling

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ while True:
     mem_info = get_mem_info()
     net_info = get_net_info()
 
-    # Configurable process count to display
-    processes = get_processes(count=7)
-
     # If os windows or linux - clear terminal
     os.system("cls" if os.name == "nt" else "clear")
 
-    # Displayed info
-    rich.print(cpu_info)
-    rich.print(mem_info)
-    rich.print(net_info)
-    print("")
-    for item in processes:
-        rich.print(item)
+    try:
+        # Configurable process count to display
+        processes = get_processes(count=7)
+
+        # Displayed info
+        rich.print(cpu_info)
+        rich.print(mem_info)
+        rich.print(net_info)
+        print("")
+        for item in processes:
+            rich.print(item)
+    except Exception as e:
+        logging.error(e) # I know it's bad idea
