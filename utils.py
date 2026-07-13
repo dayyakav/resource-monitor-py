38a823eebda39a464aa4c47c2aeffa835b17fd19
dayyakav <dayyakav@gmail.com>
2026-07-13 13:16:15 +1000

    changed get_net_info()

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def get_mem_info():
 
     return f"MEMORY : {used:.1f} GiB / {total:.0f} GiB"
 
-# Returns download and upload network info in bytes
+# Returns download and upload info in MB/s
 def get_net_info():
     prev_net = psutil.net_io_counters()
 
@@ -26,10 +26,10 @@ def get_net_info():
 
     curr_net = psutil.net_io_counters()
 
-    download = curr_net.bytes_recv - prev_net.bytes_recv
-    upload = curr_net.bytes_sent - prev_net.bytes_sent
+    download = (curr_net.bytes_recv - prev_net.bytes_recv) / 1_000_000
+    upload = (curr_net.bytes_sent - prev_net.bytes_sent) / 1_000_000
 
-    return f"NETWORK: Download : {download} B/s | Upload : {upload} B/s"
+    return f"NETWORK: Download : {download:.2f} MB/s | Upload : {upload:.2f} MB/s"
 
 # Returns list of processes with PID, name and CPU %
 def get_processes(count=10):
